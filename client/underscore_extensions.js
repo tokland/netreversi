9f6671862b97879c96fe05339788840cff173074
Arnau Sanchez <pyarnau@gmail.com>
2011-09-22 12:57:11 +0200

    add _.isNotEmpty

--- client/underscore_extensions.js
+++ client/underscore_extensions.js
@@ -1,3 +1,9 @@
+(function() {
+var node = (typeof module == "object");
+if (node) {
+  _ = require('./underscore.js');
+}
+
 _.mixin({
   /* Return a new object with the merged properties of all objects in arguments */
   merge: function() {
@@ -22,11 +28,14 @@ _.mixin({
   /* take elements from list while callback condition is met */
   takeWhile: function(list, callback, context) {
     var xs = [];
-    _.detect(list, function(item, index, list) {
+    _.any(list, function(item, index, list) {
       var res = callback.call(context, item, index, list);
-      if (res)
+      if (res) {
         xs.push(item);
-      return !res;
+        return false;
+      } else {
+        return true;
+      }
     });
     return xs;
   },
@@ -48,7 +57,7 @@ _.mixin({
   /* Return the first true element returned by the callback block (map + first) */
   mapDetect: function(list, callback, context) {
     var output;
-    _.detect(list, function(item, index, list) {
+    _.any(list, function(item, index, list) {
       var res = callback.call(context, item, index, list);
       if (res) {
         output = res;
@@ -76,13 +85,53 @@ _.mixin({
     }, []);
   },
 
+  /* Return true if object is not empty */
+  isNotEmpty: function(obj) {
+    return !_.isEmpty(obj);
+  },
+
   /* Inspect object and print it to the JS console */
   inspect: function(obj) {
     console.log(JSON.stringify(obj, null));
   }
 });
 
-//console.log(_.mapDetect([1, 2, 3, 4, 5], function(x) { if (x > 3) return 2*x }));
-//console.log(_.takeWhile([1, 2, 3, 4, 5], function(x) { return x < 3 }));
-//console.log(_.merge({a: 1}, {a: 'new1', b: 2}));
-//console.log(_(["ride", "the", "dragon"]).mash(function(s) { return [s, s.length]; }));
+if (node && process.argv[2] == "test") {
+  var assert = require('assert');
+
+  assert.deepEqual(_.merge(), {});
+  assert.deepEqual(_.merge({}), {});
+  assert.deepEqual(_.merge({a: 1}, {a: 2, b: 3}), {a: 2, b: 3});
+  assert.deepEqual(_.merge({a: 1}, {a: 2, b: 3}, {c: 4}), {a: 2, b: 3, c: 4});
+
+  assert.deepEqual(_([["a", 1], ["b", 2]]).mash(), {"a": 1, "b": 2});
+  assert.deepEqual(
+    _(["ride", "the", "dragon"]).mash(function(s) { return [s, s.length]; }),
+    {"ride": 4, "the": 3, "dragon": 6});
+
+  assert.equal(_([1, 2, 3, 4]).mapDetect(function(x) { if (x > 2) return x; }), 3);
+  assert.equal(_([1, 2, 3, 4]).mapDetect(function(x) { if (x > 10) return; }), undefined);
+
+  assert.ok(_([1, 2, 3]).containsObject(1))
+  assert.ok(!_([1, 2, 3]).containsObject(4))
+  assert.ok(_([1, [2, 3], 4]).containsObject([2, 3]))
+  assert.ok(!_([1, [2, 3], 4]).containsObject([1]))
+
+  assert.deepEqual(_.takeWhile([], function(x) { return x < 3 }), []);
+  assert.deepEqual(_.takeWhile([4, 5], function(x) { return x < 3 }), []);
+  assert.deepEqual(_.takeWhile([1, 2, 3, 4, 5], function(x) { return x < 3 }), [1, 2]);
+
+  assert.deepEqual(_.repeat("a", 0), []);
+  assert.deepEqual(_.repeat("b", 3), ["b", "b", "b"]);
+
+  assert.deepEqual(_.flatten1([]), []);
+  assert.deepEqual(_.flatten1([[1, 2], [3, 4]]), [1, 2, 3, 4]);
+  assert.deepEqual(_.flatten1([[1, 2], [[3, 4], 5]]), [1, 2, [3, 4], 5]);
+
+  assert.ok(!_([]).isNotEmpty())
+  assert.ok(_([1]).isNotEmpty())
+  assert.ok(_([1, 2, 3]).isNotEmpty())
+
+  console.log("Tests passed")
+}
+})();
